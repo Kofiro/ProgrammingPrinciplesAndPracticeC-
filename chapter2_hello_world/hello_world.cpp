31e33b7b014ee30dbc49667d7f377b73076beb58
Kofiro <kofibeduaustin222@gmail.com>
2026-05-27 16:58:47 +0000

    PractcingC++

---
+++ chapter2_hello_world/hello_world.cpp
@@ -0,0 +1,16 @@
+/**
+ * 
+ *  g++ compiler command to build and run program
+ * 
+ * g++ -I "C:\Users\kofir\Documents\C++\Practice" "chapter2_hello_world\hello_world.cpp" -o hello_world  
+ */
+
+#include "../../std_lib_facilities.h"
+
+int main() {
+   cout << "Please enter your first name (followed by 'enter'):\n";
+   string first_name;
+   cin >> first_name;
+   cout << "Hello, " << first_name << "!\n";
+   return 0;
+}
