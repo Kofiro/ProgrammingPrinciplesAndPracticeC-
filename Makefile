c4a5fd8a54526bd1a11f181719336dfe70df8b88
Kofiro <kofibeduaustin222@gmail.com>
2026-08-29 11:45:07 +0000

    revision for chapters 3

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	g++ -I "C:\Users\kofir\Documents\C++\Practice\ProgrammingPrinciplesAndPracticeC++\res" "chapter3_objects_types_values\objects_types.cpp" -o "bin\objects_types.exe"
+
+run:
+	./bin/objects_types.exe
--- chapter3_objects_types_values/objects_types.cpp
+++ chapter3_objects_types_values/objects_types.cpp
@@ -14,15 +14,39 @@
 int main() {
    // double x {2.7};
     // int y {x}; error int might narrow; double x = 2.7 would have allowed this but with {} compiler gives warning
-   string previous = " ";
-   string current;
-   int number_of_words = 0;
-   while (cin >> current) {
-    ++number_of_words;
-    if(previous == current)
-        cout << "word number " << number_of_words 
-             << " repeated word: " << current << '\n';
-    previous = current;
+   // string previous = " ";
+   // string current;
+   // int number_of_words = 0;
+   // while (cin >> current) {
+   //  ++number_of_words;
+   //  if(previous == current)
+   //      cout << "word number " << number_of_words 
+   //           << " repeated word: " << current << '\n';
+   //  previous = current;
+   //}
+
+   cout << "Input your operation and operands in the format + num num or operation operand operand \n";
+   char symbol = ' ';
+   float op1 = 0.0f;
+   float op2 = 0.0f;
+
+   cin >> symbol;
+   cin >> op1;
+   cin >> op2;
+
+   float result;
+
+   if(symbol == '+') {
+      result = op1 + op2;
+   } else if(symbol == '-') {
+      result = op1 - op2;
+   } else if (symbol == '*') {
+      result = op1 * op2;
+   } else if (symbol == '/') {
+      result = op1 / op2;
    }
+
+   cout << "Your result is " << result << '\n';
+
    return 0;
 }
